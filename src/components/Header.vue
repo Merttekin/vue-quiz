89c05a687fc0e846ce8173ea4ecc8b747e52ccec
Merttekin <merttekin94@gmail.com>
2019-06-02 13:50:21 +0300

    finished

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,20 @@
 <template>
-    <header>Header</header>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled>Quiz App</b-nav-item>
+      <b-nav-item disabled>Counter: {{numCorrect}}/{{numTotal}}</b-nav-item>
+    </b-nav>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: [
+    'numCorrect',
+    'numTotal'
+  ]
+};
 </script>
 
 <style>
-
 </style>
